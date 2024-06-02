261f6f07e1132141e4d3fed7380ee003e17c0406
tkoyama010/felupe-doc-translations <tkoyama010/felupe-doc-translations>
2024-06-02 00:11:09 +0000

    [ci skip] 25691861646 https://github.com/tkoyama010/felupe-doc-translations/actions/runs/9334095078/job/25691861646

--- locale/pot/felupe/mesh-37.py
+++ locale/pot/felupe/mesh-37.py
@@ -1,3 +1,6 @@
 import felupe as fem
 mesh = fem.mesh.RectangleArbitraryOrderQuad(order=5).add_runouts()
-mesh.plot(nonlinear_subdivision=4).show()
+mesh.plot(
+    nonlinear_subdivision=4,
+    plotter=mesh.plot(style="points", color="black"),
+).show()
--- locale/pot/felupe/mesh-38.py
+++ locale/pot/felupe/mesh-38.py
@@ -1,3 +1,6 @@
 import felupe as fem
 mesh = fem.mesh.CubeArbitraryOrderHexahedron(order=5).add_runouts()
-mesh.plot(nonlinear_subdivision=4).show()
+mesh.plot(
+    nonlinear_subdivision=4,
+    plotter=mesh.plot(style="points", color="black"),
+).show()
