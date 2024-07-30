16b809bbebfabc5c667bd356c71b3365b70aeae6
tkoyama010/felupe-doc-translations <tkoyama010/felupe-doc-translations>
2024-07-30 00:12:02 +0000

    [ci skip] 28077627810 https://github.com/tkoyama010/felupe-doc-translations/actions/runs/10153731835/job/28077627810

--- locale/pot/felupe/mesh-46.py
+++ locale/pot/felupe/mesh-46.py
@@ -1,2 +1,2 @@
-mesh_new = interpolate_line(mesh, xi=np.linspace(0, 1, 101), axis=1)
+mesh_new = fem.mesh.interpolate_line(mesh, xi=np.linspace(0, 1), axis=1)
 mesh_new.plot(style="points", color="black").show()
