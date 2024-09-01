425b16b75c2d2479ec0b9c232f32d89b59db966a
tkoyama010/felupe-doc-translations <tkoyama010/felupe-doc-translations>
2024-09-01 00:13:13 +0000

    [ci skip] 29519167091 https://github.com/tkoyama010/felupe-doc-translations/actions/runs/10649244094/job/29519167091

--- locale/pot/felupe/mechanics-6.py
+++ locale/pot/felupe/mechanics-6.py
@@ -1,13 +1,13 @@
 import felupe as fem
-mesh = fem.Cube(n=6)
-region = fem.RegionHexahedron(mesh)
-field = fem.FieldContainer([fem.Field(region, dim=3)])
-boundaries = fem.dof.symmetry(field[0])
-boundaries["clamped"] = fem.Boundary(field[0], fx=1, skip=(True, False, False))
-boundaries["move"] = fem.Boundary(field[0], fx=1, skip=(False, True, True))
-umat = fem.NeoHooke(mu=1, bulk=2)
-solid = fem.SolidBody(umat, field)
-move = fem.math.linsteps([0, 1], num=5)
-step = fem.Step(items=[solid], ramp={boundaries["move"]: move}, boundaries=boundaries)
-job = fem.Job(steps=[step]).evaluate()
-ax = solid.plot("Principal Values of Cauchy Stress").show()
+mesh = fem.mesh.Line(n=3)
+element = fem.element.Line()
+quadrature = fem.GaussLegendre(order=1, dim=1)
+region = fem.Region(mesh, element, quadrature)
+field = fem.FieldContainer([fem.Field(region, dim=1)])
+load = fem.PointLoad(field, [1, 2], values=[[3], [5]])
+vector = load.assemble.vector()
+vector.toarray()
+# Expected:
+## array([[0.],
+##        [3.],
+##        [5.]])
