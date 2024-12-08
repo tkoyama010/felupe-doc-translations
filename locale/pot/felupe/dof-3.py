1d4f9f613d2c1533426ab19ba2e4c3f4dd3217f3
tkoyama010/felupe-doc-translations <tkoyama010/felupe-doc-translations>
2024-12-08 00:14:39 +0000

    [ci skip] 34081096794 https://github.com/tkoyama010/felupe-doc-translations/actions/runs/12217179473/job/34081096794

--- locale/pot/felupe/dof-3.py
+++ locale/pot/felupe/dof-3.py
@@ -1,8 +1,2 @@
 axes = fem.Boundary(displacement, fx=0, fy=0, mode="or")
-plotter = pv.Plotter()
-actor = plotter.add_points(
-    mesh.points[axes.points],
-    point_size=20,
-    color="red",
-)
-mesh.plot(plotter=plotter, opacity=0.7).show()
+axes.plot(color="red", plotter=mesh.plot(opacity=0.7)).show()
--- locale/pot/felupe/dof-4.py
+++ locale/pot/felupe/dof-4.py
@@ -1,8 +1,2 @@
 center = fem.Boundary(displacement, fx=0, fy=0, mode="and")
-plotter = pv.Plotter()
-actor = plotter.add_points(
-    mesh.points[center.points],
-    point_size=20,
-    color="red",
-)
-mesh.plot(plotter=plotter, opacity=0.7).show()
+center.plot(color="red", plotter=mesh.plot(opacity=0.7)).show()
