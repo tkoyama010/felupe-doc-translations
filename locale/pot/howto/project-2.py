b2502c3165c30fce24a3098c125b3aa7b12302a8
tkoyama010/felupe-doc-translations <tkoyama010/felupe-doc-translations>
2025-04-04 00:13:09 +0000

    [ci skip] 39956323766 https://github.com/tkoyama010/felupe-doc-translations/actions/runs/14255135213/job/39956323766

---
+++ locale/pot/howto/project-2.py
@@ -0,0 +1,22 @@
+import pyvista as pv
+
+plotter = pv.Plotter(shape=(2, 2))
+kwargs = dict(name="Cauchy Stress", component=1, plotter=plotter)
+
+plotter.subplot(0, 0)
+kwargs_sbar = dict(interactive=False, title="Cauchy Stress YY (None)")
+solid.plot(project=None, **kwargs, scalar_bar_args=kwargs_sbar)
+
+plotter.subplot(0, 1)
+kwargs_sbar = dict(interactive=False, title="Cauchy Stress YY (topoints)")
+solid.plot(project=fem.topoints, **kwargs, scalar_bar_args=kwargs_sbar)
+
+plotter.subplot(1, 0)
+kwargs_sbar = dict(interactive=False, title="Cauchy Stress YY (project)")
+solid.plot(project=fem.project, **kwargs, scalar_bar_args=kwargs_sbar)
+
+plotter.subplot(1, 1)
+kwargs_sbar = dict(interactive=False, title="Cauchy Stress YY (extrapolate)")
+solid.plot(project=fem.tools.extrapolate, **kwargs, scalar_bar_args=kwargs_sbar)
+
+plotter.show()
